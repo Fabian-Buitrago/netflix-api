9e71c90d1d902bdba3a8f1065af8ac36ad660035
fbuitrago <fabianbuitragov@gmail.com>
2023-02-14 10:35:32 -0500

    Add user delete endPoint

--- routes/users.js
+++ routes/users.js
@@ -26,12 +26,23 @@ router.put("/:id", verifyToken, async (req, res) => {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You can update only yout account!");
+    res.status(403).json("You can update only your account!");
   }
 });
 
 //DELETE
-// router.delete("/:id", async (req, res, next) => {});
+router.delete("/:id", verifyToken, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: "User has been deleted" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can delete only your account!");
+  }
+});
 
 // //GET
 // router.get("/:id", async (req, res, next) => {});
