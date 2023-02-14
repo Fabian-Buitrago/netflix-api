d64eb736ba2e989018dcbf17127fb93a50597027
fbuitrago <fabianbuitragov@gmail.com>
2023-02-13 22:55:54 -0500

    Add register EndPoint

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
